9628c1d869e7535c5899adc2679277ed0ca686e6
Mateusz Bagiński <m.baginski@optimumpareto.com>
2024-03-10 10:59:29 +0100

    fixed __init__ imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,8 @@
-from .constants import *
-from .types import *
-from .utils import *
+from src.constants import *
+from src.types import MarkovChain, TokseqsPresentPerDoc
+from src.utils import (
+    is_list_of_strings,
+    get_distinct_elements,
+    concat_prev_toks_and_next_tok,
+    concat_tokseq,
+)
